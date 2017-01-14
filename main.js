454802c67591d8a16364deae5ad76f63f1c8db52
aaron arakawa <aaronarakawa@aarons-MacBook.local>
2017-01-13 17:57:26 -1000

    added mobile buttons

--- main.js
+++ main.js
@@ -145,12 +145,12 @@ var mainstate = {
 			playerKeys.visible = false;
 		}
 
-		if(buttonRight.input.pointerOver()){
+		if(buttonRight.input.pointerDown()){
 			player.body.velocity.x = 350;
 			console.log("right");
 		}
 
-		if(buttonLeft.input.pointerOver()){
+		if(buttonLeft.input.pointerDown()){
 			player.body.velocity.x = -350;
 			console.log("left");
 		}
